eb7bfce7fb3040421d794be57a8089e95a182848
John <jkpp0611@gmail.com>
2024-08-05 16:50:19 +0900

    day22

---
+++ day22/unemployed_community_back_end/controllers/page.js
@@ -0,0 +1,49 @@
+exports.renderMain = async (req, res, next) => {
+  try {
+    const posts = await Post.findAll({
+      include: {
+        model: User,
+        attributes: ["id", "nick"],
+      },
+      order: [["createdAt", "DESC"]],
+    });
+    res.render("main", {
+      title: "NodeBird",
+      twits: posts,
+    });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+};
+
+exports.renderHashtag = async (req, res, next) => {
+  const query = req.query.hashtag;
+  if (!query) {
+    return res.redirect("/");
+  }
+
+  try {
+    const hashtag = await Hashtag.findOne({ where: { title: query } });
+    let posts = [];
+    if (hashtag) {
+      posts = await hashtag.getPosts({ include: [{ model: User }] });
+    }
+
+    return res.render("main", {
+      title: `${query} | NodeBird`,
+      twits: posts,
+    });
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+};
+
+exports.renderJoin = (req, res) => {
+  res.render("join", { title: "Join" });
+};
+
+exports.renderMain = (req, res) => {
+  res.render("main", { title: "Main" });
+};
